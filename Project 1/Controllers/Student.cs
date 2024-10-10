86974567a3c4ba78d202151255858521d41f28a4
mohammedrashmin <mrrasmi01@gmail.com>
2024-10-10 14:07:25 +0530

    initial

---
+++ Project 1/Controllers/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_1.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Grade { get; set; }
+    }
+}
---
+++ Project 1/Controllers/StudentDpContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_1.Controllers
+{
+    public class StudentDpContext : DbContext
+    {
+        public StudentDpContext(DbContext Option<StudentDbContext>Options) : base(Option)
+        { }
+
+        public Db Set<student>students { Get();Set(); }
+
+
+    }
+
+
+}
